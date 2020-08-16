3c65833a1ff75ce4e82898414a51f2d147e43005
SeanfvdM <seanf.gsm@gmail.com>
2020-08-16 15:03:46 +0200

    Added Read for Read.java

--- DataManager/src/main/java/com/badas/datamanager/File/FileListener.java
+++ DataManager/src/main/java/com/badas/datamanager/File/FileListener.java
@@ -9,17 +9,26 @@ import java.io.File;
  */
 public interface FileListener {
     void onFileCreated(File file);
+
     void onDirectoryCreated(File directory);
+
     void onFileCreateFailed(File file);
+
     void onDirectoryCreateFailed(File directory);
+
     void onFileExists(File file);
+
     void onDirectoryExists(File directory);
+
     void onFileDeleted(File deletedFile);
+
     void onDirectoryDeleted(File deletedDirectory);
-    void onFileRead(File file, String data);
+
+    void onFileRead(File file, String data, int totalBytes);
 
     void onFileWrite(File file, String data);
 
     void onCurrentDirectoryChanged(File newDir, File oldDir);
+
     void onFileNavigate(File from, File To);
 }
--- DataManager/src/main/java/com/badas/datamanager/File/Reader.java
+++ DataManager/src/main/java/com/badas/datamanager/File/Reader.java
@@ -2,7 +2,13 @@ package com.badas.datamanager.File;
 
 import android.Manifest;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InvalidObjectException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
@@ -27,5 +33,50 @@ class Reader extends Writer {
         String[] temp = Arrays.copyOf(super.getPermission(), 2);
         temp[1] = Manifest.permission.READ_EXTERNAL_STORAGE;
         return temp;
+    }
+
+    /***
+     * Reads the file.
+     * @param file The file to be read.
+     * @return The data retrieved from the file.
+     * @throws IOException If there is an error with the file.
+     */
+    public String read(File file) throws IOException {
+        if (!file.isFile())
+            throw new InvalidObjectException(file.getName() + " is not a readable file.");
+        long length = file.length();
+        int totalBytes;
+        byte[] bytes = new byte[(int) length];
+
+        try (FileInputStream stream = new FileInputStream(file)) {
+            totalBytes = stream.read(bytes);
+        }
+
+        String contents = new String(bytes);
+        fileListener.onFileRead(file, contents, totalBytes);
+        return contents;
+    }
+
+    /***
+     * Reads the file.
+     * @param file The file to be read.
+     * @return The data retrieved from the file as a JSONObject.
+     * @throws IOException If there is an error with the file.
+     * @throws JSONException If there is an error with retrieving the JSON.
+     */
+    public JSONObject readAsJson(File file) throws IOException, JSONException {
+        if (!file.isFile())
+            throw new InvalidObjectException(file.getName() + " is not a readable file.");
+        long length = file.length();
+        int totalBytes;
+        byte[] bytes = new byte[(int) length];
+
+        try (FileInputStream stream = new FileInputStream(file)) {
+            totalBytes = stream.read(bytes);
+        }
+
+        JSONObject contents = new JSONObject(new String(bytes));
+        fileListener.onFileRead(file, contents.toString(), totalBytes);
+        return contents;
     }
 }
